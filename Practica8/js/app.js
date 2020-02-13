0debc5e75df99ac587efd7bb6977a07b745d7933
crisglz95 <cristian.gonzalez.1806@gmail.com>
2020-02-13 09:02:19 -0600

    Commit Practica 8 - Area de un triangulo con semiperimetro

---
+++ Practica8/js/app.js
@@ -0,0 +1,17 @@
+let ladoA, ladoB, ladoC, semiperimetro, area;
+
+let btnCalcular = document.getElementById('calcularArea');
+btnCalcular.addEventListener('click', generaArea);
+
+function generaArea() {
+    ladoA = Number(document.getElementById('inLadoA').value);
+    ladoB = Number(document.getElementById('inLadoB').value);
+    ladoC = Number(document.getElementById('inLadoC').value);
+
+    semiperimetro = (ladoA + ladoB + ladoC) / 2;
+
+    area = Math.sqrt((semiperimetro * (semiperimetro - ladoA)) * (semiperimetro - ladoB) * (semiperimetro - ladoC));
+
+    let mostarResultado = document.getElementById('mostrarResultado');
+    mostarResultado.innerText = 'Semiperimetro ' + semiperimetro + ' Area: ' + area + ' u2';
+}
