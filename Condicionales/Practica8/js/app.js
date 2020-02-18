6c426cf5a1e631efa5b3c4ef0472c0eb15d7aced
crisglz95 <cristian.gonzalez.1806@gmail.com>
2020-02-18 09:07:05 -0600

    Commit Practica 8 - Condicionales

---
+++ Condicionales/Practica8/js/app.js
@@ -0,0 +1,24 @@
+const button = document.getElementById('btnAnalizar');
+button.addEventListener('click', analizarTriangulo);
+
+function analizarTriangulo() {
+    const ladoMayor = Number(document.getElementById('inpLadoMayor').value);
+    const ladoA = Number(document.getElementById('inpCatetoA').value);
+    const ladoB = Number(document.getElementById('inpCatetoB').value);
+
+    const DivAlert = document.getElementById('alertAnalisis');
+
+    let sumaLados = ladoA + ladoB;
+
+    if (ladoMayor > sumaLados) {
+        DivAlert.innerText = 'No se puede formar un triangulo';
+    } else {
+        if (ladoMayor == ladoA && ladoA == ladoB && ladoMayor == ladoB) {
+            DivAlert.innerText = 'Triangulo Equilatero';
+        } else if (ladoMayor != ladoA && ladoA != ladoB && ladoMayor != ladoB) {
+            DivAlert.innerText = 'Triengulo Escaleno';
+        } else {
+            DivAlert.innerText = 'Triangulo Isoseles';
+        }
+    }
+}
