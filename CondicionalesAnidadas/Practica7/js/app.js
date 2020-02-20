b7178466659d152365bf15f2ff2b1c8db08e33dd
crisglz95 <cristian.gonzalez.1806@gmail.com>
2020-02-20 10:42:32 -0600

    Reduccion de codigo practica 7

--- CondicionalesAnidadas/Practica7/js/app.js
+++ CondicionalesAnidadas/Practica7/js/app.js
@@ -5,18 +5,16 @@ function calcularHoras() {
     const horas = Number(document.getElementById('inpHoras').value);
     const printAlert = document.getElementById('imprimirAlerta');
 
-    let restante, totalPagar, horasAux;
+    let restante, totalPagar, horasAux, divImprimir;
 
     if (horas < 3) {
         totalPagar = horas * 5;
-        let divImprimir = `<div class="alert alert-success mt-3">Total a pagar: ${totalPagar}</div>`;
-        printAlert.innerHTML = divImprimir;
+        divImprimir = `<div class="alert alert-success mt-3">Total a pagar: ${totalPagar}</div>`;
     } else if (horas > 2 && horas < 6) {
         restante = horas - 2;
         totalPagar = ((horas - restante) * 5);
         totalPagar = (restante * 4) + totalPagar;
-        let divImprimir = `<div class="alert alert-success mt-3">Total a pagar: ${totalPagar}</div>`;
-        printAlert.innerHTML = divImprimir;
+        divImprimir = `<div class="alert alert-success mt-3">Total a pagar: ${totalPagar}</div>`;
     } else if (horas > 5 && horas < 11) {
         restante = horas - 2;
         totalPagar = ((horas - restante) * 5);
@@ -24,8 +22,7 @@ function calcularHoras() {
         restante = restante - 3;
         totalPagar = ((horasAux - restante) * 4) + totalPagar;
         totalPagar = (restante * 3) + totalPagar;
-        let divImprimir = `<div class="alert alert-success mt-3">Total a pagar: ${totalPagar}</div>`;
-        printAlert.innerHTML = divImprimir;
+        divImprimir = `<div class="alert alert-success mt-3">Total a pagar: ${totalPagar}</div>`;
     } else {
         restante = horas - 2;
         totalPagar = ((horas - restante) * 5);
@@ -36,7 +33,7 @@ function calcularHoras() {
         restante = restante - 5;
         totalPagar = ((horasAux - restante) * 3) + totalPagar;
         totalPagar = totalPagar + (restante * 2);
-        let divImprimir = `<div class="alert alert-success mt-3">Total a pagar: ${totalPagar}</div>`;
-        printAlert.innerHTML = divImprimir;
+        divImprimir = `<div class="alert alert-success mt-3">Total a pagar: ${totalPagar}</div>`;
     }
+    printAlert.innerHTML = divImprimir;
 }
